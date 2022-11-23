79688b29eaa62746049a1d6efcc2f768a69dc4c8
권율 <rnjsdbf95@gmail.com>
2022-11-23 14:58:08 +0900

    12

--- 9-5.py
+++ 9-5.py
@@ -2,6 +2,8 @@ class Dog:
     def __init__(self,name):
         self.name = name
         
-    def bark():
-        print("멍멍")
+    def bark(self):
+        print("내 이름은 {} 멍멍".format(self.name))
         
+my_dog = Dog('jindo')
+my_dog.bark()
