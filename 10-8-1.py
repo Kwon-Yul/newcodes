d568477bb593786f79f0108ab442cc445a33286c
권율 <rnjsdbf95@gmail.com>
2022-11-24 16:23:37 +0900

    10

---
+++ 10-8-1.py
@@ -0,0 +1,10 @@
+txt = "Welcome to Busan Metropolitan City"
+print(txt.split())
+# 실행 결과는 ['Welcome', 'to', 'Busan', 'Metropolitan', 'City'] 로 나옵니다.
+
+greet = "Hello,My name is Dongmin,Good to see you again"
+print(greet.split(','))
+# 실행 결과는 ['Hello', 'My name is Dongmin', 'Good to see you again'] 로 나옵니다.
+
+fruits = 'Apple|Banana|Melon|Orange'
+print(fruits.split('|'))
